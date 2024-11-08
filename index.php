a12a08903f14682472483b691721bd03fb7cd9b1
vinicius <viniciusfpress@gmail.com>
2024-11-08 20:58:03 -0300

    primeirocomit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>Lista de tafera </title>
+  </head>
+  <body>
+    
+    <h1 class="text-center py-4 my-4">Lista de tarefa</h1>
+
+    <div class="w-50 m-auto">
+    <form action="data.php" method="post" autocomplete="off">
+        <div class="form-group">
+            <label for="title">Titulo</label>
+            <input class="form-control" type="text" name="title" id="title" placeholder="..." Required>
+
+        </div><br>
+        <button class="btn btn-success">Adiciona sua tarefa</button>
+
+    </form>
+
+    </div><br>
+    <hr class="bg-dark w-50 m-auto">
+    <div class="lists w-50 m-auto my-4">
+        <h1>Sua lista</h1>
+        <div id="lists">
+        <table class="table table-dark table-hover">
+  <thead>
+    <tr>
+      <th scope="col" name="id">id</th>
+      <th scope="col">lista de tarefa</th>
+    <th>Ações</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+        include 'database.php';
+        $sql="SELECT * FROM todos";
+        $result=mysqli_query($conn, $sql);
+
+        if($result){
+            while($row=mysqli_fetch_assoc($result)){
+                $id=$row['id'];
+                $title=$row['title'];
+               
+
+
+                ?>
+                <tr>
+                    <td><?php echo $id ?></td>
+                    <td><?php echo $title  ?></td>
+                    <td>
+                    <a class="btn btn-success btn-sm" href="edit.php?id=<?php echo $id ?>" role="button">Edit</a>
+                    <a class="btn btn-danger btn-sm" href="delete.php?id=<?php echo $id ?>" role="button">Delete</a>
+ 
+                    </td>
+                      
+                </tr>
+
+                <?php
+
+                
+            }
+        }
+    ?>
+    
+   
+  </tbody>
+</table>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
---
+++ todos.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 09/11/2024 às 00:54
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `todos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `todos`
+--
+
+CREATE TABLE `todos` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `todos`
+--
+
+INSERT INTO `todos` (`id`, `title`) VALUES
+(4, 'vinixua '),
+(5, 'vinxi');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `todos`
+--
+ALTER TABLE `todos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `todos`
+--
+ALTER TABLE `todos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
